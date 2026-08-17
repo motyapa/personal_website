a4e23f149eb296d4db3aecf7f1fe59c872845972
Arthur Vartanyan <arthurva@uw.edu>
2026-08-16 20:54:04 -0700

    fix cors parsing

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("CORS_ORIGINS")],
+    allow_origins=os.getenv("CORS_ORIGINS", "").split(","),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
